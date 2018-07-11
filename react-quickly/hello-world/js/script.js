e834e343867f404c64c15d58f53c2525ec46e15d
Min Xu <xukmin@gmail.com>
2018-07-11 15:48:11 -0700

    Create a <div> element with 2 <h1> children.

--- react-quickly/hello-world/js/script.js
+++ react-quickly/hello-world/js/script.js
@@ -1,4 +1,5 @@
+const h1 = React.createElement('h1', null, 'Hello world!');
 ReactDOM.render(
-  React.createElement('h1', null, 'Hello world!'),
+  React.createElement('div', null, h1, h1),
   document.getElementById('content')
 );
