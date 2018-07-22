cbb6e458604d6053fc37ea67aa5cf936ef080d28
Min Xu <xukmin@gmail.com>
2018-07-21 23:53:27 -0700

    Hello React.js!

--- react-quickly/hello-world/js/script.jsx
+++ react-quickly/hello-world/js/script.jsx
@@ -1,6 +1,6 @@
 class HelloWorld extends React.Component {
   render() {
-    return <h1 {...this.props}>
+    return <h1>
       Hello, {this.props.frameworkName} world!
     </h1>;
   }
@@ -9,19 +9,9 @@ class HelloWorld extends React.Component {
 ReactDOM.render(
     <div>
       <HelloWorld
-        id='ember'
-        frameworkName='Ember.js'
+        id='react'
+        frameworkName='React.js'
         title='A framework for creating ambitious web applications.'
-      />
-      <HelloWorld
-        id='backbone'
-        frameworkName='Backbone.js'
-        title='Backbone.js gives structure to web applications.'
-      />
-      <HelloWorld
-        id='angular'
-        frameworkName='Angular.js'
-        title='Superheroic JavaScript MVW Framework.'
       />
       Current time is <Clock/>
     </div>,
