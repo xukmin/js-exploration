96bce6f743fb3724c26ec2a4580dcc0be6329963
Min Xu <xukmin@gmail.com>
2018-07-30 22:26:41 -0700

    Add a Button component.

---
+++ react-quickly/event-handling/jsx/button.jsx
@@ -0,0 +1,17 @@
+class SaveButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleSave(event) {
+    console.log(this, event);
+  }
+
+  render() {
+    return <button onClick={this.handleSave}>
+      Save
+    </button>;
+  }
+}
+
