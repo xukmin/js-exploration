f31965a9eac0caff9728f016a050221195cfb0cf
Min Xu <xukmin@gmail.com>
2018-07-31 22:00:38 -0700

    Make Button change state when clicked.

--- react-quickly/event-handling/jsx/button.jsx
+++ react-quickly/event-handling/jsx/button.jsx
@@ -1,16 +1,24 @@
 class SaveButton extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    };
     this.handleSave = this.handleSave.bind(this);
   }
 
   handleSave(event) {
     console.log(this, event);
+    this.setState((prevState, props) => {
+      return Object.assign(prevState, {
+        count: prevState.count + 1
+      });
+    });
   }
 
   render() {
     return <button onClick={this.handleSave}>
-      Save
+      Saved {this.state.count} {this.state.count == 1 ? 'time' : 'times'}
     </button>;
   }
 }
