483fdb6a849f1406dc5c05972a089c69a1fbbd2f
Min Xu <xukmin@gmail.com>
2018-08-01 00:27:15 -0700

    Add mouse events.
    Also add CSS.

--- react-quickly/event-handling/jsx/button.jsx
+++ react-quickly/event-handling/jsx/button.jsx
@@ -2,12 +2,15 @@ class SaveButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      className: 'inactive'
     };
-    this.handleSave = this.handleSave.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
   }
 
-  handleSave(event) {
+  onClick(event) {
     console.log(this, event);
     this.setState((prevState, props) => {
       return Object.assign(prevState, {
@@ -16,8 +19,28 @@ class SaveButton extends React.Component {
     });
   }
 
+  onMouseOver(event) {
+    this.setState((prevState, props) => {
+      return Object.assign(prevState, {
+        className: 'active'
+      });
+    });
+  }
+
+  onMouseOut(event) {
+    this.setState((prevState, props) => {
+      return Object.assign(prevState, {
+        className: 'inactive'
+      });
+    });
+  }
+
   render() {
-    return <button onClick={this.handleSave}>
+    return <button
+        onClick={this.onClick}
+        onMouseOver={this.onMouseOver}
+        onMouseOut={this.onMouseOut}
+        className={this.state.className}>
       Saved {this.state.count} {this.state.count == 1 ? 'time' : 'times'}
     </button>;
   }
