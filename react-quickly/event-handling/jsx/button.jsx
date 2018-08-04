fdacb3ca93062dbd38d0b27351b379f1c15503dd
Min Xu <xukmin@gmail.com>
2018-08-03 22:51:38 -0700

    Handle window resize event in Button.

--- react-quickly/event-handling/jsx/button.jsx
+++ react-quickly/event-handling/jsx/button.jsx
@@ -3,11 +3,22 @@ class SaveButton extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      className: 'inactive'
+      className: 'inactive',
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight
     };
     this.onClick = this.onClick.bind(this);
     this.onMouseOver = this.onMouseOver.bind(this);
     this.onMouseOut = this.onMouseOut.bind(this);
+    this.onResize = this.onResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
   }
 
   onClick(event) {
@@ -35,13 +46,23 @@ class SaveButton extends React.Component {
     });
   }
 
+  onResize(event) {
+    this.setState((prevState, props) => {
+      return Object.assign(prevState, {
+        windowWidth: window.innerWidth,
+        windowHeight: window.innerHeight
+      });
+    });
+  }
+i
   render() {
     return <button
         onClick={this.onClick}
         onMouseOver={this.onMouseOver}
         onMouseOut={this.onMouseOut}
         className={this.state.className}>
-      Clicked {this.state.count} {this.state.count == 1 ? 'time' : 'times'}
+      Clicked {this.state.count} {this.state.count == 1 ? 'time' : 'times'}<br/>
+      Current Window size is {this.state.windowWidth}x{this.state.windowHeight}
     </button>;
   }
 }
